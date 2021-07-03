c1f7fef76719f55682cd2652c051f32a1193de7d
RakeshSR10 <rakeshsrking@gmail.com>
2021-07-03 19:45:40 +0530

    feat: server is listening to the port number

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("Welcome to Address Book Back-End Application")
+const express = require('express');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Address Book MERN"});
+});
+
+// listen for requests
+app.listen(4000, () => {
+    console.log("Server is listening on port 4000");
+});
