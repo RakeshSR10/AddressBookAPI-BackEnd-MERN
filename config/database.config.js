773a54f4b1685889da36f743784a7a1a7cea988a
RakeshSR10 <rakeshsrking@gmail.com>
2021-07-03 20:01:15 +0530

    feat: successfully connecting to the mongoDB

---
+++ config/database.config.js
@@ -0,0 +1,10 @@
+/**
+ * @file          database.config.js
+ * @description   connection details of the mongoDB
+ * @author        Rakesh S R <rakeshsrking@gmail.com>
+ * @since         03/07/2021
+ */
+
+ module.exports = {
+    url: 'mongodb://localhost:27017/address-book-app'
+}
--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(express.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(express.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Address Book MERN"});
