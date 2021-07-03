b9300bfa9395adc94670f369b9c516477b183492
RakeshSR10 <rakeshsrking@gmail.com>
2021-07-03 20:29:04 +0530

    feat: added dotenv

--- config/database.config.js
+++ config/database.config.js
@@ -5,6 +5,20 @@
  * @since         03/07/2021
  */
 
- module.exports = {
-    url: 'mongodb://localhost:27017/address-book-app'
-}
+ const mongoose = require("mongoose");
+ require('dotenv').config();
+ 
+ mongoose.Promise = global.Promise;
+ 
+ // Connecting to the database
+ mongoose.connect(process.env.URL, { 
+     useNewUrlParser: true, 
+     useUnifiedTopology: true,
+     useCreateIndex: true,
+     useCreateIndex: true,
+     useFindAndModify: false
+     })
+     .then(() => { console.log("Successfully connected to the database"); })
+     .catch(err => { console.log('Could not connect to the database. Exiting now...', err);
+                     process.exit();
+ });
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+require('dotenv').config();
 
 // create express app
 const app = express();
@@ -9,22 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(express.json())
 
-// Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+const port = process.env.PORT
 
 // define a simple route
 app.get('/', (req, res) => {
@@ -32,6 +20,8 @@ app.get('/', (req, res) => {
 });
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log("Server is listening on port 4000");
 });
+
+module.exports = app;
